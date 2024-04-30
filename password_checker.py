d46edec60c6f3b520e51075bcaea5904e0b32b46
michal <michklak91@gmail.com>
2024-04-30 08:44:02 +0200

    updated app checker

--- password_checker.py
+++ password_checker.py
@@ -1,3 +1,7 @@
 username = input('username: ')
 password = input('password: ')
-print(username + ', your password ' + '*' * len(password) + ' is ' + str(len(password)) + ' letter long')
+print(username + ', your password ' + '*' * len(password) + ', is ' + str(len(password)) + ' letter long')
+
+password_length = len(password)
+hidden_password = '*' * password_length
+print(f'{username}, your password {hidden_password}, is {password_length} letters long')
