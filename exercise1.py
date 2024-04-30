b8d961b207c4ff719b9b8672c11c26a37d8b551e
michal <michklak91@gmail.com>
2024-04-30 08:39:45 +0200

    password checker added

--- exercise1.py
+++ exercise1.py
@@ -1,3 +1,3 @@
 from datetime import datetime
-age = int(datetime.now().year)  - int(input('what is your year birth? '))
+age = int(datetime.now().year) - int(input('what is your year birth? '))
 print("you are " + str(age) + " years old")
---
+++ password_checker.py
@@ -0,0 +1,3 @@
+username = input('username: ')
+password = input('password: ')
+print(username + ', your password ' + '*' * len(password) + ' is ' + str(len(password)) + ' letter long')
