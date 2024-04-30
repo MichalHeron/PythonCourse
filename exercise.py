48ee77912157ce4ee8a3634df309fedcfaf984ee
michal <michklak91@gmail.com>
2024-04-30 12:27:07 +0200

    terrnary operator

--- exercise.py
+++ exercise.py
@@ -1,15 +1,21 @@
-basket = ['a', 'b', 'c', 'd', 'e']
+# basket = ['a', 'b', 'c', 'd', 'e']
 
 
-def new_func():
-    return 'a' in basket
+# def new_func():
+#     return 'a' in basket
 
 
-print(new_func())
+# print(new_func())
 
 
-# unpacking
-a, b, c, *other, d = [1, 2, 3, 4, 5, 6, 7, 8]
-print(a)
-print(b)
-print(other)
+# # unpacking
+# a, b, c, *other, d = [1, 2, 3, 4, 5, 6, 7, 8]
+# print(a)
+# print(b)
+# print(other)
+
+
+# ternary operator
+is_friend = True
+can_message = "message allowed" if is_friend else "not message"
+print(can_message)
