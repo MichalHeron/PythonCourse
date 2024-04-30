01ea111413d34fd4b8d85daa72be4e339ac9ef80
michal <michklak91@gmail.com>
2024-04-30 12:37:05 +0200

    exercise logical

--- exercise.py
+++ exercise.py
@@ -16,6 +16,21 @@
 
 
 # ternary operator
-is_friend = True
-can_message = "message allowed" if is_friend else "not message"
-print(can_message)
+# is_friend = True
+# can_message = "message allowed" if is_friend else "not message"
+# print(can_message)
+
+# exercise logical
+is_magician = False
+is_expert = True
+
+if is_magician and is_expert:
+    print("you are a master magician")
+
+elif is_magician and not is_expert:
+    print("at least you're getting there")
+
+elif not is_magician:
+    print("you need magic powers")
+
+# exercise logical
