f4f6cbd46fa407b298508cc1a483d99add859a7f
michal <michklak91@gmail.com>
2024-05-01 18:01:24 +0200

    Exercise: Check for duplicates in list:

--- exercise1.py
+++ exercise1.py
@@ -14,19 +14,31 @@
 
 #Exercise!
 #Display the image below to the right hand side where the 0 is going to be ' ', and the 1 is going to be '*'. This will reveal an image!
-picture = [
-    [0, 0, 0, 1, 0, 0, 0],
-    [0, 0, 1, 1, 1, 0, 0],
-    [0, 1, 1, 1, 1, 1, 0],
-    [1, 1, 1, 1, 1, 1, 1],
-    [0, 0, 0, 1, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0]
-]
+# picture = [
+#     [0, 0, 0, 1, 0, 0, 0],
+#     [0, 0, 1, 1, 1, 0, 0],
+#     [0, 1, 1, 1, 1, 1, 0],
+#     [1, 1, 1, 1, 1, 1, 1],
+#     [0, 0, 0, 1, 0, 0, 0],
+#     [0, 0, 0, 1, 0, 0, 0]
+# ]
+#
+# for i in picture:
+#     for pixel in i:
+#         if pixel:
+#             print('*', end='')
+#         else:
+#             print(' ', end='')
+#     print()
 
-for i in picture:
-    for j in i:
-        if j == 1:
-            print('*', end='')
-        else:
-            print(' ', end='')
-    print()
+# Exercise: Check for duplicates in list:
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+
+duplicates = []
+
+for value in some_list:
+    if some_list.count(value) > 1:
+        if value not in duplicates:
+            duplicates.append(value)
+
+print(duplicates)
