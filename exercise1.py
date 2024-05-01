1aad2c20dbca388715d3e10a7005dcbe0352caf0
michal <michklak91@gmail.com>
2024-05-01 14:38:29 +0200

    enumerate

--- exercise1.py
+++ exercise1.py
@@ -1,4 +1,13 @@
-from datetime import datetime
-age = int(datetime.now().year) - int(input('what is your year birth? '))
-print("you are " + str(age) + " years old")
+# from datetime import datetime
+# age = int(datetime.now().year) - int(input('what is your year birth? '))
+# print("you are " + str(age) + " years old")
+#
 
+# enumerate
+
+for i, j in enumerate(['a', 'b', 'c']):
+    print(i, j)
+
+for i, j in enumerate(list(range(100))):
+    if j == 50:
+        print(i, j)
