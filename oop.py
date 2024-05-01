242b30faedc2d02541bf4682812918c6fced40b0
michal <michklak91@gmail.com>
2024-05-01 21:22:56 +0200

    cat exercise 2

--- oop.py
+++ oop.py
@@ -15,24 +15,85 @@
 
 
 #Given the below class:
-class Cat:
-    species = 'mammal'
+# class Cat:
+#     species = 'mammal'
+#
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#
+# # 1 Instantiate the Cat object with 3 cats
+# cat1 = Cat('Tom', 5)
+# cat2 = Cat('Jerry', 4)
+# cat3 = Cat('Sally', 3)
+#
+#
+# # 2 Create a function that finds the oldest cat
+# def find_oldest_cat(*cats):
+#     return max(cat.age for cat in cats)
+#
+#
+# # 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
+# print(f'The oldest cat is {find_oldest_cat(cat1, cat2, cat3)} years old.')
+
+# another cat exercise
+
+class Pets():
+    animals = []
+
+    def __init__(self, animals):
+        self.animals = animals
+
+    def walk(self):
+        for animal in self.animals:
+            print(animal.walk())
+
+
+class Cat():
+    is_lazy = True
 
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
-
-# 1 Instantiate the Cat object with 3 cats
-cat1 = Cat('Tom', 5)
-cat2 = Cat('Jerry', 4)
-cat3 = Cat('Sally', 3)
+    def walk(self):
+        return f'{self.name} is just walking around'
 
 
-# 2 Create a function that finds the oldest cat
-def find_oldest_cat(*cats):
-    return max(cat.age for cat in cats)
+class Simon(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
 
 
-# 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
-print(f'The oldest cat is {find_oldest_cat(cat1, cat2, cat3)} years old.')
+class Sally(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+
+#1 Add another Cat
+
+class Tikachu(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+
+#2 Create a list of all of the pets (create 3 cat instances from the above)
+my_cats = []
+cat1 = Simon('Simon', 10)
+cat2 = Sally('Sally', 15)
+cat3 = Tikachu('Tikachu', 5)
+
+my_cats.append(cat1)
+my_cats.append(cat2)
+my_cats.append(cat3)
+
+#OR my_cats = [Simon('Simon', 10), Sally('Sally', 15), Tikachu('Tikachu', 5)]
+
+#3 Instantiate the Pet class with all your cats use variable my_pets
+
+my_pets = Pets(my_cats)
+
+#4 Output all of the cats walking using the my_pets instance
+
+my_pets.walk()
