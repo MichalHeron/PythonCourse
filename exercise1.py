ac8f0ab69172a0a5f93893170e48ebc56484f3de
michal <michklak91@gmail.com>
2024-05-01 17:49:11 +0200

    exercise christmas tree

--- exercise1.py
+++ exercise1.py
@@ -5,9 +5,28 @@
 
 # enumerate
 
-for i, j in enumerate(['a', 'b', 'c']):
-    print(i, j)
+# for i, j in enumerate(['a', 'b', 'c']):
+#     print(i, j)
+#
+# for i, j in enumerate(list(range(100))):
+#     if j == 50:
+#         print(i, j)
 
-for i, j in enumerate(list(range(100))):
-    if j == 50:
-        print(i, j)
+#Exercise!
+#Display the image below to the right hand side where the 0 is going to be ' ', and the 1 is going to be '*'. This will reveal an image!
+picture = [
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0]
+]
+
+for i in picture:
+    for j in i:
+        if j == 1:
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print()
