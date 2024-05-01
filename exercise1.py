cc63ddf2b579d2160136595ad98683b0b688f039
michal <michklak91@gmail.com>
2024-05-01 18:26:28 +0200

    tesla exercise

--- exercise1.py
+++ exercise1.py
@@ -32,13 +32,37 @@
 #     print()
 
 # Exercise: Check for duplicates in list:
-some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+# some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+#
+# duplicates = []
+#
+# for value in some_list:
+#     if some_list.count(value) > 1:
+#         if value not in duplicates:
+#             duplicates.append(value)
+#
+# print(duplicates)
 
-duplicates = []
+# tesla exercise
 
-for value in some_list:
-    if some_list.count(value) > 1:
-        if value not in duplicates:
-            duplicates.append(value)
+age_input = input("What is your age?: ")
 
-print(duplicates)
+
+def check_driver_age(age="0"):
+    if int(age) < 18:
+        print("Sorry, you are too young to drive this car. Powering off")
+    elif int(age) > 18:
+        print("Powering On. Enjoy the ride!")
+    elif int(age) == 18:
+        print("Congratulations on your first year of driving. Enjoy the ride!")
+
+
+check_driver_age(age_input)
+
+#1. Wrap the above code in a function called checkDriverAge(). Whenever you call this function, you will get prompted for age.
+# Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime?
+
+#2 Instead of using the input(). Now, make the checkDriverAge() function accept an argument of age, so that if you enter:
+#checkDriverAge(92);
+#it returns "Powering On. Enjoy the ride!"
+#also make it so that the default age is set to 0 if no argument is given.
